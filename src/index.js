5236b37eb6dcb465dbfd53ef2dd47692a7ff5b7b
Karol Tatała <ktatala@wikia-inc.com>
2017-11-06 22:40:49 +0100

    ad test

--- src/index.js
+++ src/index.js
@@ -57,7 +57,8 @@ window.wikiaJWPlayer = function (elementId, options, callback) {
 				advertising: {
 					autoplayadsmuted: willAutoplay,
 					client: 'googima',
-					vpaidcontrols: true
+					vpaidcontrols: true,
+					tag: 'https://pubads.g.doubleclick.net/gampad/ads?output=xml_vast3&env=vp&gdfp_req=1&unviewed_position_start=1&iu=%2F5441%2Fwka1a.VIDEO%2Ffeatured%2Fdesktop%2Foasis-fv-article%2F_project43-life&sz=640x480&url=http%3A%2F%2Fsandbox-xw1.project43.wikia.com%2Fwiki%2FSyntheticTests%2FPremium%2FFeaturedVideo%2FCoD%3Flog_level%3Ddebug%26log_group%3Dwikia.articleVideo.featuredVideo.ads&description_url=http%3A%2F%2Fsandbox-xw1.project43.wikia.com%2Fwiki%2FSyntheticTests%2FPremium%2FFeaturedVideo%2FCoD%3Flog_level%3Ddebug%26log_group%3Dwikia.articleVideo.featuredVideo.ads&correlator=1531892329411442&cust_params=wsi%3Doxax%26s0%3Dlife%26s0v%3Dlifestyle%26s0c%3Dtech%26s1%3D_project43%26s2%3Dfv-article%26ab%3D52_170%26ar%3D3%3A4%26artid%3D451%26dmn%3Dwikiacom%26hostpre%3Dsandbox-xw1%26skin%3Doasis%26lang%3Den%26wpage%3Dsynthetictests%2Fpremium%2Ffeaturedvideo%2Fcod%26ref%3Ddirect%26esrb%3Dteen%26geo%3DUS%26pv%3D21%26u%3Dsc3g051dx%26ksgmnt%3D%26top%3D1k%26passback%3Djwplayer%26pos%3DFEATURED%26rv%3D1%26src%3Dtest&vpos=preroll&vid_t=Synthetic%20green%20(16%3A9)&sdkv=h.3.184.1&sdki=3c0d&scor=3861023004270693&adk=87358093&u_so=l&osd=2&frm=0&sdr=1&vpa=auto&mpt=jwplayer&mpv=8.0.0&afvsz=200x200%2C250x250%2C300x250%2C336x280%2C450x50%2C468x60%2C480x70&ged=ve4_td11_tt2_pd11_la1000_er353.25.505.325_vi196.0.1089.756_vp100_eb24171'
 				},
 				autostart: willAutoplay && !document.hidden,
 				description: options.videoDetails.description,
@@ -89,6 +90,10 @@ window.wikiaJWPlayer = function (elementId, options, callback) {
 		playerInstance.setup(playerSetup);
 		logger.info('after setup');
 		logger.subscribeToPlayerErrors(playerInstance);
+
+		playerInstance.on('all', function (event) {
+			console.log(event);
+		});
 
 		return playerInstance;
 	}
