c187cd5e17460c560b1608359ca4656ef8c9bc0b
bkoval <bartlomiej.kowalczyk@wikia-inc.com>
2018-01-26 15:43:07 +0100

    Komcie | Budowa #7

--- src/scripts/annotation/index.js
+++ src/scripts/annotation/index.js
@@ -3,7 +3,6 @@ import Comment from './Comments';
 import AnnotationStream from './AnnotationStream';
 
 export default function wikiaJWPlayerAnnotation(playerInstance, { annotations, comments, spoilers }) {
-	console.log('#######', '', annotations, comments); 
 	new AnnotationStream(playerInstance, { amount: 3 })
 		.add(annotations.map((el) => new Annotation({ item: el, playerInstance })))
 		.add(comments.map((el) => new Comment({ item: el, playerInstance })));
